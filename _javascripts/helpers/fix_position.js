424996358d066fdfaed1df0867388e5d4a75d03e
Ludovic Turmel <ludovic.turmel@liip.ch>
2016-11-04 17:54:55 +0100

    Remove console log & center icon arrow up

--- _javascripts/helpers/fix_position.js
+++ _javascripts/helpers/fix_position.js
@@ -2,17 +2,14 @@ export default class FixPosition {
   constructor() {
     var timeout;
     let node = document.querySelector('.boat-sidebar');
-    console.log(window.scrollY);
 
     window.addEventListener('scroll', function() {
       if (timeout) clearTimeout(timeout);
       timeout = setTimeout(function(){
 
         if (window.scrollY > 450) {
-          console.log('ture');
           node.classList.add('fixed');
         } else {
-          console.log('false');
           node.classList.remove('fixed');
         }
       }, 0);
